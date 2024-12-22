55b6209988c78cec2b54127c3a59c5e894315203
rashed-mia <rashedkamal2565@gmail.com>
2024-12-22 14:55:02 +0600

    This is a simple new year landing page.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        merriweather: ['Merriweather', 'serif'], 
+        inter: ['Inter', 'serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
